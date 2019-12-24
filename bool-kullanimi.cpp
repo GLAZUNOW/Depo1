15a079fafc677efb2681537506280c4c9bc037d5
Yunus Emre Öztürk <oz2rkk@hotmail.com>
2019-12-24 21:34:37 +0300

    7.Ders

---
+++ bool-kullanimi.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	bool X,Y,Z;
+	X = (20 < 15);
+	Y = (1 == 13);
+	Z = (23 != 25);
+	
+	cout << X <<endl;
+	cout<< Y <<endl;
+	cout <<Z;
+	
+	cin>>X;
+}
